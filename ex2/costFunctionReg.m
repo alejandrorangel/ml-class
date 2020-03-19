b379b82eeeaa0480ccf8414a21b3136d018cab7a
Alejandro Rangel <arangel.glz@gmail.com>
2020-03-18 21:40:11 -0700

    complete ex2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,11 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
+penalize = sum(theta(2:end) .^ 2);
+J = J + lambda/(2*m) * penalize;
 
-
-
-
-
+grad(2:end) = grad(2:end) + (lambda/m)*theta(2:end);
 % =============================================================
 
 end
